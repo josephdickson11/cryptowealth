4c92553367f220ca282fbf31033da50937d07f82
mname <uche.chinedu@e4email.net>
2021-12-01 01:17:39 +0100

    add total accounts lists

--- apis/version1/route_accounts.py
+++ apis/version1/route_accounts.py
@@ -1,25 +1,42 @@
-from fastapi import APIRouter
-from sqlalchemy.orm import Session
-from fastapi import Depends, HTTPException, status 
+from typing import List
 
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import status
+from sqlalchemy.orm import Session
+
+from db.repository.normal_account import create_new_account
+from db.repository.normal_account import list_accounts
+from db.repository.normal_account import retrieve_account
 from db.session import get_db
-from db.models.normal_account import Normal_accounts
-from schemas.normal_account import AccountCreate, ShowAccount
-from db.repository.normal_account import create_new_account, retrieve_account
+from schemas.normal_account import AccountCreate
+from schemas.normal_account import ShowAccount
+
+# from db.models.normal_account import Normal_accounts
 
 router = APIRouter()
 
 
-@router.post("/create-account/",response_model=ShowAccount)
-def create_account(account: AccountCreate,db: Session = Depends(get_db)):
+@router.post("/create-account/", response_model=ShowAccount)
+def create_account(account: AccountCreate, db: Session = Depends(get_db)):
     current_customer = 1
-    account = create_new_account(account=account,db=db,customer_id=current_customer)
+    account = create_new_account(account=account, db=db, customer_id=current_customer)
     return account
 
 
 @router.get("/get/{id}", response_model=ShowAccount)
-def read_account(id:int, db:Session = Depends(get_db)):
+def read_account(id: int, db: Session = Depends(get_db)):
     account = retrieve_account(id=id, db=db)
     if not account:
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"account with id {id} does not exist")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"account with id {id} does not exist",
+        )
     return account
+
+
+@router.get("/all", response_model=List[ShowAccount])
+def read_accounts(db: Session = Depends(get_db)):
+    accounts = list_accounts(db=db)
+    return accounts
--- db/repository/normal_account.py
+++ db/repository/normal_account.py
@@ -1,16 +1,22 @@
 from sqlalchemy.orm import Session
 
-from schemas.normal_account import AccountCreate
 from db.models.normal_account import Normal_accounts
+from schemas.normal_account import AccountCreate
 
 
-def create_new_account(account: AccountCreate, db: Session, customer_id:int):
+def create_new_account(account: AccountCreate, db: Session, customer_id: int):
     account_obj = Normal_accounts(**account.dict(), customer_id=customer_id)
     db.add(account_obj)
     db.commit()
     db.refresh(account_obj)
     return account_obj
 
-def retrieve_account(id:int, db:Session):
+
+def retrieve_account(id: int, db: Session):
     item = db.query(Normal_accounts).filter(Normal_accounts.id == id).first()
     return item
+
+
+def list_accounts(db: Session):
+    accounts = db.query(Normal_accounts).all()
+    return accounts
