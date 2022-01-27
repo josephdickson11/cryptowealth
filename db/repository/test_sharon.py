90326acc7afb9e5e7d79aa6fa25e669ffc809995
mname <uche.chinedu@e4email.net>
2022-01-27 15:02:14 +0100

    adding client tutorials

---
+++ db/repository/test_sharon.py
@@ -0,0 +1,45 @@
+account_balance = 670
+account_name = "Rani"
+account_type = "savings"
+indebt = False
+
+
+import datetime
+
+x = datetime.datetime(2018, 9, 15)
+
+
+# print(datetime.date.today())
+
+yesterday = datetime.datetime(2022, 1, 13)
+print(yesterday)
+
+today = datetime.date.today()
+print(today)
+
+
+def credit_account():
+    account_balance = 670
+    account_name = "Rani"
+    account_type = "savings"
+    indebt = False
+    is_verified = False
+    account_limit = 5000
+
+    print("current account balance is : ", account_balance)
+    amount_to_credit = int(input("please enter amount to credit: "))
+
+    if amount_to_credit > account_limit:
+        if is_verified:
+            account_balance = account_balance + amount_to_credit
+            print("credit successful, your new account balance is", account_balance)
+        else:
+            print("kindly verify account and try again")
+    else:
+        account_balance = account_balance + amount_to_credit
+        print("credit successful, your new account balance is", account_balance)
+
+    return account_balance
+
+
+credit_account()
